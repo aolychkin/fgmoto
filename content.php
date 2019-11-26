c9962e854dc5aea892eee7d1e6d8a2763b7d43e6
Alexey Lychkin <aolychkin@gmail.com>
2019-11-26 22:32:46 +0300

    create project

---
+++ content.php
@@ -0,0 +1,22 @@
+<section class="product-single">
+    <div class="product-single img">
+        <img src="static/img/mitas_type.jpg" alt="">
+    </div>
+    <div class="product-single filter">
+        <h1>Шипованная резина MITAS</h1>
+        <div class="price_range">
+            <h3>от 11 200 руб.</h3>
+            <h3>до 28 800 руб.</h3>
+        </div>
+<!--        =========================  Блок с выбором кубатуры  =========================           -->
+        <div class="product-single filter-block">
+            <h3>Выберите кубатуру.</h3>
+            <div class="blocks">
+                <div class="block">
+                    <h4>85 CC</h4>
+                    <p>R18/R19/R21</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php
+require "function.php";
+require "header.php";
+require "content.php";
+require "footer.php";
