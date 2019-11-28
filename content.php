a4652a5f608c67f28722df575704e086b4536ed9
Alexey Lychkin <aolychkin@gmail.com>
2019-11-28 13:19:10 +0300

    frontend

--- content.php
+++ content.php
@@ -1,22 +1,83 @@
 <section class="product-single">
-    <div class="product-single img">
-        <img src="static/img/mitas_type.jpg" alt="">
+    <div class="product-single-img">
+        <figure class="img"></figure>
     </div>
-    <div class="product-single filter">
+    <div class="product-single-filter">
         <h1>Шипованная резина MITAS</h1>
         <div class="price_range">
             <h3>от 11 200 руб.</h3>
             <h3>до 28 800 руб.</h3>
         </div>
-<!--        =========================  Блок с выбором кубатуры  =========================           -->
-        <div class="product-single filter-block">
+        <!--        =========================  Блок с выбором кубатуры  =========================           -->
+        <div class="product-single-filter-block">
             <h3>Выберите кубатуру.</h3>
             <div class="blocks">
+                <div class="block active">
+                    <h4>85 CC</h4>
+                    <p>R18/R19/R21</p>
+                </div>
+                <div class="block">
+                    <h4>85 CC</h4>
+                    <p>R18/R19/R21</p>
+                </div>
+                <div class="block">
+                    <h4>85 CC</h4>
+                    <p>R18/R19/R21</p>
+                </div>
+                <div class="block">
+                    <h4>85 CC</h4>
+                    <p>R18/R19/R21</p>
+                </div>
                 <div class="block">
                     <h4>85 CC</h4>
                     <p>R18/R19/R21</p>
                 </div>
             </div>
         </div>
+        <!--        =========================  Блок с выбором типа  =========================           -->
+        <div class="product-single-filter-block">
+            <h3>Выберите тип.</h3>
+            <div class="blocks">
+                <div class="block active">
+                    <h4>Комплект</h4>
+                    <p>Резин</p>
+                </div>
+                <div class="block">
+                    <h4>Передняя</h4>
+                    <p>Резина</p>
+                </div>
+                <div class="block">
+                    <h4>Задняя</h4>
+                    <p>Резина</p>
+                </div>
+            </div>
+        </div>
+        <!--        =========================  Блок с выбором радиуса  =========================           -->
+        <div class="product-single-filter-block">
+            <h3>Выберите радиус резины.</h3>
+            <div class="blocks">
+                <div class="blocks-left">
+                    <div class="block active">
+                        <h4>18 R</h4>
+                        <p>Передняя</p>
+                    </div>
+                </div>
+                <div class="blocks-right">
+                    <div class="block">
+                        <h4>18 R</h4>
+                        <p>Задняя</p>
+                    </div>
+                    <div class="block active">
+                        <h4>19 R</h4>
+                        <p>Задняя</p>
+                    </div>
+                    <div class="block">
+                        <h4>21 R</h4>
+                        <p>Задняя</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </section>
--- header.php
+++ header.php
@@ -5,6 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="static/font/montserrat.css">
+    <link rel="stylesheet" href="style/main.css">
     <title>Document</title>
 </head>
 <body>
+<header>
+    <img src="static/svg/logo.svg" alt="">
+</header>
+<figure class="wave"></figure>
